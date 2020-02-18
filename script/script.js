38bcfbbc51263ee0a0d60908b8f0bc9b94a30273
Tomasz <Tomasz.Cudzich@icloud.com>
2020-02-18 21:01:02 +0100

    Initialize repository

---
+++ script/script.js
@@ -0,0 +1,29 @@
+function clock(){
+	var today = new Date();
+
+	var day = today.getDate();
+		if(day<10) day = "0"+day
+
+	var month = today.getMonth()+1;
+	if(month<10) month = "0"+month
+
+	var year = today.getFullYear();
+
+	var hours = today.getHours();
+	if(hours<10) hours = "0"+hours
+
+	var minutes = today.getMinutes();
+		if(minutes<10) minutes = "0"+minutes
+
+	var seconds = today.getSeconds();
+		if(seconds<10) seconds = "0"+seconds
+
+	document.getElementById("clock").innerHTML = hours+":"+minutes+":"+seconds+" <br/> "+day+"."+month+"."+year;
+
+	setTimeout("clock()" , 1000)
+
+}
+
+window.onload = function(){
+	clock();
+}
